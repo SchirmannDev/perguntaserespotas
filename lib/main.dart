27c63a8cac43b61a264dd85df57604c1d6097d06
schirmann <cschirmann.dev@gmail.com>
2021-05-18 17:08:14 -0300

    iniciando app com hellow flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(new PerguntaApp());
-}
+main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello Flutter'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text('Perguntas'),
+          ),
+        ),
+        body: Text('Hellow FLutter'),
+      ),
     );
   }
 }
