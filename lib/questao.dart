5406f8d42ae876357f6bbf9f5c5501870ca87e9d
schirmann <cschirmann.dev@gmail.com>
2021-05-26 22:08:53 -0300

    criando estilos

--- lib/questao.dart
+++ lib/questao.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Questao extends StatelessWidget {
-  
   final String texto;
 
   Questao(this.texto);
 
   @override
   Widget build(BuildContext context) {
-    return Text(texto);
+    return Container(
+      child: Text(
+        texto,
+        style: TextStyle(
+          fontSize: 28,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
-
-
