43299197b525f89a21aa3c9adf760259bd50b6cc
schirmann <cschirmann.dev@gmail.com>
2021-06-01 16:56:24 -0300

    parametros

--- lib/questionario.dart
+++ lib/questionario.dart
@@ -5,6 +5,14 @@ class Questionario extends StatelessWidget {
   final int perguntaSelecionada;
   final void Function() responder;
 
+  Questionario({
+    @required this.perguntas,
+     @required this.perguntaSelecionada,
+     @required this.responder
+
+    
+  })
+
   @override
   Widget build(BuildContext context) {
     return Column(
