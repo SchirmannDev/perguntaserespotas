5b5c5905abc02cb23c6eb5db60758422690f21d2
schirmann <cschirmann.dev@gmail.com>
2021-06-01 16:49:24 -0300

    widget questionario

--- lib/questionario.dart
+++ lib/questionario.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 
 class Questionario extends StatelessWidget {
+  final List<Map<String, Object>> perguntas;
+  final int perguntaSelecionada;
+  final void Function() responder;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        Questao(_perguntas[_perguntaSelecionada]['texto']),
+        ...respostas.map((t) => Resposta(t, _responder)).toList()
+      ],
+    );
   }
 }
